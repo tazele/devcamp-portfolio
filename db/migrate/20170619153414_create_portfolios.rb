ed8140e46ad85da3baf8296c81f1dacae1400102
Jordan Hudgens <jordan@devcamp.com>
2017-06-19 15:39:59 +0000

    Added porfolio items via Resource generator

---
+++ db/migrate/20170619153414_create_portfolios.rb
@@ -0,0 +1,13 @@
+class CreatePortfolios < ActiveRecord::Migration[5.0]
+  def change
+    create_table :portfolios do |t|
+      t.string :titile
+      t.string :subtitle
+      t.text :body
+      t.text :main_image
+      t.text :thum_image
+
+      t.timestamps
+    end
+  end
+end
