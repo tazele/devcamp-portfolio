90b064c10e71bca6cd101743abd1b12d78b7d7b4
Jordan Hudgens <jordan@devcamp.com>
2017-07-18 14:41:30 +0000

    integrated now gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -55,3 +55,5 @@ gem 'friendly_id', '~> 5.1.0'
 gem 'devise', '~> 4.3'
 
 gem 'bootstrap', '~>4.0.0.alpha6'
+
+gem 'devcamp_view_tool', git:'https://github.com/tazele/devcamp_view_tool'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+  
+  before_action :set_copyright
+  
+  def set_copyright
+    @copyright = DevcampViewTool::Renderer.copyright 'Anastasia Armstrong', 'All rights reserved'
+  end
 end
