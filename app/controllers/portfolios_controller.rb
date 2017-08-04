d73c148a709b3610f5cbf2bb4606cbed1f138111
Jordan Hudgens <jordan@devcamp.com>
2017-08-04 19:04:48 +0000

    implemmented persistent position updates for drag and drop interface

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,74 +1,78 @@
 class PortfoliosController < ApplicationController
-  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
-  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   
   def index
     @portfolio_items = Portfolio.by_position
   end
-  
-  def angular
-    @angular_portfolio_items = Portfolio.all
+
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
+
+    render nothing: true
   end
-  
-  def new 
+
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
+
+  def new
     @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build }
   end
-  
+
   def create
-   @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Your Portfolio Item is Now Live.' }
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
     end
   end
-  
+
   def edit
   end
-  
+
   def update
-    
     respond_to do |format|
-      if @blog.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-        format.html { redirect_to portfolios_path, notice: 'The Record was successfully updated.' }
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
     end
   end
-  
+
   def show
   end
-  
+
   def destroy
-    #Preform the lookup
-    @portfolio_item = Portfolio.find(params[:id])
-    
-    #destroy/delet the record
+    # Destroy/delete the record
     @portfolio_item.destroy
-    
+
     # Redirect
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Post was successfully removed.' }
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
-  
+
+  private
+
   def portfolio_params
     params.require(:portfolio).permit(:title,
                                       :subtitle,
                                       :body,
                                       technologies_attributes: [:name]
-                                      )
+                                     )
   end
-  
+
   def set_portfolio_item
     @portfolio_item = Portfolio.find(params[:id])
   end
 end
-  
- 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,7 +26,7 @@ module ApplicationHelper
         title: 'Home'
       },
       {
-        url: about_path,
+        url: about_me_path,
         title: 'About Me'
       },
       {
