5bde47fe57fc1453ddd764ec0bc05636015faaea
Jordan Hudgens <jordan@devcamp.com>
2017-08-06 21:29:42 +0000

    integrated cocoon for portfolio itmens nested form component

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,8 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
+//= require jquery-ui
+//= require html.sortable
+//= require cocoon
 //= require turbolinks
 //= require_tree .
